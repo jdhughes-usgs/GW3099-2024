94b8b2a17c046d971cad367e3bb383a5b0dd738c
Joseph Hughes <jdhughes@usgs.gov>
2024-08-25 13:40:01 -0500

    Enforce ruff formatting on python scripts and jupyter notebooks

--- .scripts/test_notebooks.py
+++ .scripts/test_notebooks.py
@@ -8,17 +8,21 @@ DIRS = (
     pl.Path("../exercises-completed/parallel/").resolve(),
 )
 
+
 def get_notebook_paths(dir_path):
     return sorted(f.name for f in dir_path.glob("*.ipynb"))
+
 
 def run_cmd(cmd):
     print(f"Running command: {' '.join(cmd)}")
     proc = subprocess.run(cmd)
     assert proc.returncode == 0, f"Error running command: {' '.join(cmd)}"
 
+
 def run_notebook(nb_name):
     cmd = ("jupytext", "--execute", f"{nb_name}")
     run_cmd(cmd)
+
 
 if __name__ == "__main__":
     for idx, dir_path in enumerate(DIRS):
