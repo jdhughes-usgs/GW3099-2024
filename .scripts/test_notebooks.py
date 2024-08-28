aeb7c0c2d2aac53075e416b1830a242c7bbc4430
jdhughes-usgs <jdhughes@usgs.gov>
2024-08-28 13:55:13 -0500

    Add 1st netcdf example based on watershed model (#10)
    There is a known failure on linux with the conda environment that we are trying to work out.

--- .scripts/test_notebooks.py
+++ .scripts/test_notebooks.py
@@ -1,17 +1,35 @@
+import argparse
 import os
 import pathlib as pl
 import subprocess
+import sys
 
 ROOT_DIR = pl.Path(os.getcwd()).resolve()
 DIRS = (
     pl.Path("../exercises-completed/flopy/").resolve(),
     pl.Path("../base/watershed/").resolve(),
     pl.Path("../exercises-completed/parallel/").resolve(),
+    pl.Path("../exercises-completed/netcdf/").resolve(),
 )
+SKIP_NOTEBOOKS = {
+    "step0_netcdf_output": ("win32",),
+    "mf6minsim-plot": ("win32", "darwin", "linux"),
+}
 
 
 def get_notebook_paths(dir_path):
-    return sorted(f.name for f in dir_path.glob("*.ipynb"))
+    names = sorted(f"{f.name}" for f in dir_path.glob("*.ipynb"))
+    select_names = []
+    os_name = sys.platform.lower()
+    for name in names:
+        add_name = True
+        for key, values in SKIP_NOTEBOOKS.items():
+            if key == pl.Path(name).stem and os_name in values:
+                add_name = False
+                break
+        if add_name:
+            select_names.append(name)
+    return tuple(select_names)
 
 
 def run_cmd(cmd):
@@ -25,10 +43,35 @@ def run_notebook(nb_name):
     run_cmd(cmd)
 
 
+def run_script(nb_name):
+    py_script = "test.py"
+    cmd = ("jupytext", "--output", f"{py_script}", f"{nb_name}")
+    run_cmd(cmd)
+
+    cmd = ("python", f"{py_script}")
+    run_cmd(cmd)
+
+    pl.Path(py_script).unlink()
+
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Test gw3099 completed notebooks."
+    )
+    parser.add_argument(
+        "-s",
+        "--script",
+        action="store_true",
+        help="Convert notebooks to scripts",
+    )
+    args = parser.parse_args()
+
     for idx, dir_path in enumerate(DIRS):
         nb_paths = get_notebook_paths(dir_path)
         os.chdir(dir_path)
         for p in nb_paths:
-            success = run_notebook(p)
+            if args.script:
+                run_script(p)
+            else:
+                run_notebook(p)
         os.chdir(ROOT_DIR)
--- .scripts/update_pc_files.py
+++ .scripts/update_pc_files.py
@@ -1,28 +1,35 @@
 import os
 import pathlib as pl
+import subprocess
 
-conda_path = os.environ.get("CONDA_PREFIX", None)
-if conda_path is not None:
-    conda_path = pl.Path(conda_path)
-    netcdf_path = conda_path / "lib/pkgconfig/netcdf-fortran.pc"
-    if netcdf_path.is_file():
-        with open(netcdf_path, "r") as f:
-            lines = f.readlines()
-        tag = "fmoddir="
-        update = False
-        for idx, line in enumerate(lines):
-            if tag in line:
-                test = line.strip()
-                if test == tag:
-                    update = True
-                    lines[idx] = line.replace(
-                        tag, f"{tag}{conda_path / 'include'}"
-                    )
-                break
-        if update:
-            print(f"Updating {tag} with the include path.")
-            with open(netcdf_path, "w") as f:
-                for line in lines:
-                    f.write(line)
-        else:
-            print("All files are in good shape.")
+nc_includepath = pl.Path(
+    subprocess.check_output(("nf-config", "--includedir")).decode().strip()
+)
+nc_libpath = nc_includepath.parent / "lib"
+print(
+    f"netcdf-fortran include path: {nc_includepath}\n"
+    + f"netcdf-fortran lib path:     {nc_libpath}"
+)
+
+nc_pcpath = nc_libpath / "pkgconfig/netcdf-fortran.pc"
+if nc_pcpath.is_file():
+    with open(nc_pcpath, "r") as f:
+        lines = f.readlines()
+    tag = "fmoddir="
+    update = False
+    for idx, line in enumerate(lines):
+        if tag in line:
+            test = line.strip()
+            if test == tag:
+                update = True
+                lines[idx] = line.replace(tag, f"{tag}{nc_includepath}")
+            break
+    if update:
+        print(f"Updating {tag} with the include path.")
+        with open(nc_pcpath, "w") as f:
+            for line in lines:
+                f.write(line)
+    else:
+        print("All files are in good shape.")
+else:
+    print(f"netcdf-fortran pkgconfig file '{nc_pcpath}' was not found")
